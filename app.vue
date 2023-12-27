a9c560e42c6ddc5979be3fdfdf02907ab0f631d6
andresk-devt <learningxandres@gmail.com>
2023-12-26 19:56:11 -0500

    Organice the i18n internacionalization mode in the project

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <NuxtWelcome />
-  </div>
-</template>
-
-<style lang="scss">
-@import '/assets/scss/_main.scss';
-</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="home-container">
+    <h1>Hola mundo!</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../assets/scss/_main.scss';
+</style>
