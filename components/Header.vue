5772c202aa36b657b71ab2cb2352bcf8b8325a92
andresk-devt <learningxandres@gmail.com>
2023-12-27 10:27:56 -0500

    Organice the component header

---
+++ components/Header.vue
@@ -0,0 +1,69 @@
+<script setup></script>
+
+<template>
+  <div class="header-container">
+    <div class="title-container">
+      <h1 class="title-container__text">
+        {{ "< Andres" }}<span>{{ "Gomez />" }}</span>
+      </h1>
+    </div>
+    <nav class="menu-container">
+      <ul class="menu-container-list">
+        <li class="menu-container-list__item">Home</li>
+        <li class="menu-container-list__item">Services</li>
+        <li class="menu-container-list__item">Projects</li>
+        <li class="menu-container-list__item">Contact</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header-container {
+  display: flex;
+  justify-content: space-around;
+  height: 65px;
+  background-color: $tertiary-color;
+}
+.title-container {
+  display: flex;
+  align-items: center;
+  &__text {
+    font-family: $font-portfolio;
+    color: $text-color;
+    font-size: 1.3rem;
+    span {
+      color: $secondary-color;
+    }
+  }
+}
+.menu-container {
+  display: flex;
+  align-items: center;
+  &-list {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    gap: 20px;
+    &__item {
+      color: $text-color;
+      font-family: $font-portfolio;
+      font-weight: 300;
+      cursor: pointer;
+    }
+  }
+}
+
+@media (max-width: 750px) {
+  .header-container {
+    justify-content: center;
+    padding: 0px 30px;
+  }
+  .menu-container-list {
+    gap: 25px;
+  }
+  .title-container {
+    display: none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,32 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <div class="home-container">
-    <div class="language-selector">
-      <LanguageSelector />
+    <div class="header-container">
+      <Header />
+      <div class="language-selector">
+        <LanguageSelector />
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-@import '../assets/scss/_main.scss';
+@import "../assets/scss/_main.scss";
 
 .home-container {
   position: relative;
+  background-color: $primary-color;
+}
+
+.header-container {
+  position: sticky;
+  top: 0;
 }
 
 .language-selector {
   position: absolute;
-  right: 1%;
-  top: 5px;
+  right: 2%;
+  top: 9px;
 }
-
 </style>
